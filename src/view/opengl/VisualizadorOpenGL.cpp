60826a6a8d6801ec02041ec8ddb8dd9ddc802026
tiago-rods <t.alves.rodrigues@hotmail.com>
2026-09-16 19:12:18 -0300

    feat: Implementação parcial do visualizador (falta poucas coisas)

--- src/view/opengl/VisualizadorOpenGL.cpp
+++ src/view/opengl/VisualizadorOpenGL.cpp
@@ -1,14 +1,80 @@
 #include "view/opengl/VisualizadorOpenGL.hpp"
+#include <GL/freeglut.h>
+
+void VisualizadorOpenGL::inicializarJanela(){
+    // depois anotar o que cada coisa faz
+    instancia_ = this;
+
+    int argc = 1;
+    char nomePrograma[] = "CuboMagico2x2";
+    char* argv[] = { nomePrograma };
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Cubo Magico 2x2x2");
+
+    glEnable(GL_DEPTH_TEST);
+    glMatrixMode(GL_PROJECTION);
+    gluPerspective(45.0, 800.0 / 600.0, 0.1, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+
+    glutDisplayFunc(callbackDesenhar);
+    glutMouseFunc(callbackMouseClick);
+    glutMotionFunc(callbackMouseArrasto);
+
+    janelaCriada_ = true;
+}
+
+void VisualizadorOpenGL::desenharCena(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    camera_.aplicar();
+
+    // TODO (D.3/D.4): trocar por 8 cubies posicionados/coloridos conforme estadoAtual_.
+    glutSolidCube(1.0);
+
+    glutSwapBuffers();
+}
+
+void VisualizadorOpenGL::callbackDesenhar() {
+    if (instancia_) instancia_->desenharCena();
+}
+
+void VisualizadorOpenGL::callbackMouseClick(int botao, int estado, int x, int y) {
+    if (!instancia_ || botao != GLUT_LEFT_BUTTON) return;
+
+    instancia_->arrastando_ = (estado == GLUT_DOWN);
+    instancia_->mouseX_ = x;
+    instancia_->mouseY_ = y;
+}
+
+void VisualizadorOpenGL::callbackMouseArrasto(int x, int y) {
+    if (!instancia_ || !instancia_->arrastando_) return;
+
+    int deltaX = x - instancia_->mouseX_;
+    int deltaY = y - instancia_->mouseY_;
+    instancia_->camera_.arrastar(deltaX, deltaY);
+    instancia_->mouseX_ = x;
+    instancia_->mouseY_ = y;
+
+    glutPostRedisplay();
+}
 
 void VisualizadorOpenGL::renderizar(const EstadoCubo& estado) {
-    // TODO (Semana 3): janela freeglut, 8 cubies coloridos via glutSolidCube.
+    estadoAtual_ = estado;
+
+    if(!janelaCriada_) inicializarJanela;
+
+    glutPostRedisplay();
+    glutMainLoopEvent();
 }
 
 Comando VisualizadorOpenGL::lerComando() {
-    // TODO (Semana 3)
+    // TODO (D.5)
     return Comando{};
 }
 
 void VisualizadorOpenGL::mostrarMensagem(const std::string& mensagem) {
-    // TODO (Semana 3)
+    // TODO (D.5/D.6)
 }
--- src/view/opengl/VisualizadorOpenGL.hpp
+++ src/view/opengl/VisualizadorOpenGL.hpp
@@ -1,12 +1,27 @@
 #pragma once
 
 #include "view/IVisualizador.hpp"
-
-// TODO (Semana 3): renderizar os 8 cubies em 3D com freeglut (pipeline fixo).
-// So compilado quando WITH_OPENGL=ON no CMake.
+#include "view/opengl/Camera.hpp"
 class VisualizadorOpenGL : public IVisualizador {
 public:
     void renderizar(const EstadoCubo& estado) override;
     Comando lerComando() override;
     void mostrarMensagem(const std::string& mensagem) override;
+
+private: 
+    void inicializarJanela();
+    void desenharCena();
+
+    static void callbackDesenhar();
+    static void callbackMouseClick(int botao, int estado, int x, int y);
+    static void callbackMouseArrasto(int x, int y);
+
+    Camera camera_;
+    EstadoCubo estadoAtual_;
+    bool janelaCriada_ = false;
+    bool arrastando_ = false;
+    int mouseX_ = 0;
+    int mouseY_ = 0;
+
+    static VisualizadorOpenGL* instancia_; // erro nessa linah ver o motivo
 };
